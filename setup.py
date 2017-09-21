b8fd472b1056ce58faf4826f307ef1b694dac771
John Martin <john.martin@configure.systems>
2017-09-21 12:52:21 -0500

    changed package upload to pypi.org instead of python.org due to change

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 
 setup(name='salesforce-analytics',
-      version='0.1.3b',
+      version='0.1.3c',
       description="""Client to injest SalesForce Case information and generate
       analytics for trends and current statistics for evaluating on the
       fly""",
